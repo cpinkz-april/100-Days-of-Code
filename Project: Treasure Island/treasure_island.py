652b2d36edb945db27eba032f773dc8aa0c2605a
cpinkz-april <cp.aethaya@gmail.com>
2025-06-16 09:04:44 +0700

    Update treasure_island.py

--- Project: Treasure Island/treasure_island.py
+++ Project: Treasure Island/treasure_island.py
@@ -8,8 +8,12 @@ def treasure_island():
         print("Game over!")
     if direction == "Left" or direction == "left":
         verb = input("Will you swim or wait?")
+        if verb == "Wait" or verb == "wait":
+            print("Game over!")
         if verb == "Swim" or "swim":
             door_color = input("Congrats! You've reached the final destination. You know a treasure is hidden behind one of these doors. But there are 3 doors with different colors, 'Red' 'Yellow' 'Blue'. You're making the decision... Which color?")
+            if door_color == "Red" or door_color =="red" or door_color == "Blue" or door_color == "blue":
+                print("You open the door. But sadly there's no any treasure!!")
             if door_color == "yellow" or "Yellow":
                 print("You win! You've found the gold!!")
 
